caa46ecf304fe07381cf4a007a0029c37b7318ae
hahoho87 <hahoho87@gmail.com>
2021-05-02 22:54:27 +0900

    add ObjectProvider for Dependency Lookup

--- src/test/java/com/devbaek/springcore/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/devbaek/springcore/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,8 @@ package com.devbaek.springcore.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -41,18 +43,17 @@ class SingletonWithPrototypeTest {
         int count2 = clientBean2.logic();
         // then
         assertThat(count1).isEqualTo(1);
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
